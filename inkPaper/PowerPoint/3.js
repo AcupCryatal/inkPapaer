d786dcb8905d4d631e2491fd127a2ae7178800ea
AcupCryatal <1021956996@qq.com>
2021-08-24 13:57:21 +0800

    first

---
+++ inkPaper/PowerPoint/3.js
@@ -0,0 +1,16 @@
+function openCity(cityName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(cityName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
+// 触发 id="defaultOpen" click 事件
+document.getElementById("defaultOpen").click();
